d670bf00115fba080feed7f8ef121ee8e6e949c1
Elias-exe <adventurergamer11@hotmail.com>
2022-05-23 13:49:28 -0300

    fix: httpclient code repetition

--- fe/src/services/utils/HttpClient.js
+++ fe/src/services/utils/HttpClient.js
@@ -5,30 +5,29 @@ class HttpClient {
     this.baseURL = baseURL;
   }
 
-  async get(path) {
-    const response = await fetch(`${this.baseURL}${path}`);
-
-    let body = null;
-    const contentType = response.headers.get('Content-Type');
-    if (contentType.includes('application/json')) {
-      body = await response.json();
-    }
-
-    if (response.ok) {
-      return body;
-    }
-
-    throw new APIError(response, body);
+  get(path) {
+    return this.makeRequest(path, {
+      method: 'GET',
+    });
   }
 
-  async post(path, body) {
-    const headers = new Headers({
-      'Content-Type': 'application/json',
-    });
-
-    const response = await fetch(`${this.baseURL}${path}`, {
+  post(path, body) {
+    return this.makeRequest(path, {
       method: 'POST',
       body: JSON.stringify(body),
+    });
+  }
+
+  async makeRequest(path, options) {
+    const headers = new Headers();
+
+    if (options.body) {
+      headers.append('Content-Type', 'application/json');
+    }
+
+    const response = await fetch(`${this.baseURL}${path}`, {
+      method: options.method,
+      body: JSON.stringify(options.body),
       headers,
     });
 
