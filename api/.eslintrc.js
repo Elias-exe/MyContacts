7db964546ce4c8626bcaa6dd02172423165165c9
Elias-exe <adventurergamer11@hotmail.com>
2022-05-23 15:48:55 -0300

    fix: improving the code

---
+++ api/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'class-methods-use-this': 'off',
+    'consistent-return': 'off',
+    camelcase: 'off',
+    'no-promise-executor-return': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: next }],
+  },
+};
---
+++ api/src/app/controllers/ContactController.js
@@ -0,0 +1,116 @@
+const ContactsRepository = require('../repositories/ContactsRepository');
+const isValidUUID = require('../utils/isValidUUID');
+
+class ContactController {
+  async index(request, response) {
+    const { orderBy } = request.query;
+    const contacts = await ContactsRepository.findAll(orderBy);
+
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.json(contacts);
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    if(!isValidUUID(id)){
+      return response.status(404).json({ error: 'Invalid contact id!' });
+    }
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'Contact not found' });
+    }
+
+    response.json(contact);
+  }
+
+  async store(request, response) {
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if(category_id && !isValidUUID(category_id)){
+      return response.status(404).json({ error: 'Invalid category id!' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if(email){
+      if (contactExists) {
+        return response.status(400).json({ error: 'This e-mail is already been taken' });
+      }
+    }
+
+    const contact = await ContactsRepository.create({
+      name,
+      email: email || null,
+      phone,
+      category_id: category_id || null,
+    });
+
+    response.status(201).json(contact);
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+
+    if(!isValidUUID(id)){
+      return response.status(404).json({ error: 'Invalid contact id!' });
+    }
+
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    if(category_id && !isValidUUID(category_id)){
+      return response.status(404).json({ error: 'Invalid category id!' });
+    }
+
+    const contactExists = await ContactsRepository.findById(id);
+
+    if (!contactExists) {
+      return response.status(404).json({ error: 'Contact not found' });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+
+    if(email){
+      if (contactByEmail && contactByEmail.id !== id) {
+        return response.status(400).json({ error: 'This e-mail is in use' });
+      }
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name,
+      email: email|| null,
+      phone,
+      category_id: category_id || null,
+    });
+    response.json(contact);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    if(!isValidUUID(id)){
+      return response.status(404).json({ error: 'Invalid contact id!' });
+    }
+
+    await ContactsRepository.delete(id);
+
+    response.sendStatus(204);
+  }
+}
+
+// Singleton
+module.exports = new ContactController();
